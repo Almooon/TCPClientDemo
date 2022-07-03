369aa8d6948aee35f6a5398e1c939332578aeea1
wmh9039 <wmh9039@DESKTOP-JT1C141>
2022-07-03 17:26:06 +0800

    添加项目文件。

---
+++ TCPClientDemo/TCPDemoForm.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace TCPClientDemo
+{
+    partial class TCPDemoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbReadtimeout = new System.Windows.Forms.TextBox();
+            this.tbInterval = new System.Windows.Forms.TextBox();
+            this.rbrn = new System.Windows.Forms.RadioButton();
+            this.rbn = new System.Windows.Forms.RadioButton();
+            this.rbr = new System.Windows.Forms.RadioButton();
+            this.btnSendBt = new System.Windows.Forms.Button();
+            this.lbTimer = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDiscon = new System.Windows.Forms.Button();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.btnReceive = new System.Windows.Forms.Button();
+            this.btnSendStr = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lbReceived = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbCMD = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.tbIP = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.tbReadtimeout);
+            this.panel1.Controls.Add(this.tbInterval);
+            this.panel1.Controls.Add(this.rbrn);
+            this.panel1.Controls.Add(this.rbn);
+            this.panel1.Controls.Add(this.rbr);
+            this.panel1.Controls.Add(this.btnSendBt);
+            this.panel1.Controls.Add(this.lbTimer);
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Controls.Add(this.btnDiscon);
+            this.panel1.Controls.Add(this.lbStatus);
+            this.panel1.Controls.Add(this.btnReceive);
+            this.panel1.Controls.Add(this.btnSendStr);
+            this.panel1.Controls.Add(this.btnConnect);
+            this.panel1.Controls.Add(this.lbReceived);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.tbCMD);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.tbPort);
+            this.panel1.Controls.Add(this.tbIP);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(429, 388);
+            this.panel1.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(108, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "RTO:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "ITV:";
+            // 
+            // tbReadtimeout
+            // 
+            this.tbReadtimeout.Location = new System.Drawing.Point(143, 39);
+            this.tbReadtimeout.Name = "tbReadtimeout";
+            this.tbReadtimeout.Size = new System.Drawing.Size(68, 21);
+            this.tbReadtimeout.TabIndex = 19;
+            // 
+            // tbInterval
+            // 
+            this.tbInterval.Location = new System.Drawing.Point(37, 39);
+            this.tbInterval.Name = "tbInterval";
+            this.tbInterval.Size = new System.Drawing.Size(54, 21);
+            this.tbInterval.TabIndex = 18;
+            // 
+            // rbrn
+            // 
+            this.rbrn.AutoSize = true;
+            this.rbrn.Location = new System.Drawing.Point(284, 57);
+            this.rbrn.Name = "rbrn";
+            this.rbrn.Size = new System.Drawing.Size(47, 16);
+            this.rbrn.TabIndex = 17;
+            this.rbrn.TabStop = true;
+            this.rbrn.Text = "\\r\\n";
+            this.rbrn.UseVisualStyleBackColor = true;
+            this.rbrn.CheckedChanged += new System.EventHandler(this.OnRadiobuttonChecked);
+            // 
+            // rbn
+            // 
+            this.rbn.AutoSize = true;
+            this.rbn.Location = new System.Drawing.Point(284, 35);
+            this.rbn.Name = "rbn";
+            this.rbn.Size = new System.Drawing.Size(35, 16);
+            this.rbn.TabIndex = 16;
+            this.rbn.TabStop = true;
+            this.rbn.Text = "\\n";
+            this.rbn.UseVisualStyleBackColor = true;
+            this.rbn.CheckedChanged += new System.EventHandler(this.OnRadiobuttonChecked);
+            // 
+            // rbr
+            // 
+            this.rbr.AutoSize = true;
+            this.rbr.Location = new System.Drawing.Point(284, 13);
+            this.rbr.Name = "rbr";
+            this.rbr.Size = new System.Drawing.Size(35, 16);
+            this.rbr.TabIndex = 15;
+            this.rbr.TabStop = true;
+            this.rbr.Text = "\\r";
+            this.rbr.UseVisualStyleBackColor = true;
+            this.rbr.CheckedChanged += new System.EventHandler(this.OnRadiobuttonChecked);
+            // 
+            // btnSendBt
+            // 
+            this.btnSendBt.Location = new System.Drawing.Point(193, 126);
+            this.btnSendBt.Name = "btnSendBt";
+            this.btnSendBt.Size = new System.Drawing.Size(103, 23);
+            this.btnSendBt.TabIndex = 14;
+            this.btnSendBt.Text = "Send by bytes";
+            this.btnSendBt.UseVisualStyleBackColor = true;
+            this.btnSendBt.Click += new System.EventHandler(this.btnSendBt_Click);
+            // 
+            // lbTimer
+            // 
+            this.lbTimer.Location = new System.Drawing.Point(185, 359);
+            this.lbTimer.Name = "lbTimer";
+            this.lbTimer.Size = new System.Drawing.Size(151, 18);
+            this.lbTimer.TabIndex = 13;
+            this.lbTimer.Text = "Connecting: 0";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(342, 68);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDiscon
+            // 
+            this.btnDiscon.Location = new System.Drawing.Point(342, 39);
+            this.btnDiscon.Name = "btnDiscon";
+            this.btnDiscon.Size = new System.Drawing.Size(75, 23);
+            this.btnDiscon.TabIndex = 11;
+            this.btnDiscon.Text = "DisCon.";
+            this.btnDiscon.UseVisualStyleBackColor = true;
+            this.btnDiscon.Click += new System.EventHandler(this.btnDiscon_Click);
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.ForeColor = System.Drawing.Color.Brown;
+            this.lbStatus.Location = new System.Drawing.Point(12, 359);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(101, 18);
+            this.lbStatus.TabIndex = 10;
+            this.lbStatus.Text = "Offline";
+            // 
+            // btnReceive
+            // 
+            this.btnReceive.Location = new System.Drawing.Point(344, 354);
+            this.btnReceive.Name = "btnReceive";
+            this.btnReceive.Size = new System.Drawing.Size(75, 23);
+            this.btnReceive.TabIndex = 9;
+            this.btnReceive.Text = "Receive";
+            this.btnReceive.UseVisualStyleBackColor = true;
+            this.btnReceive.Click += new System.EventHandler(this.btnReceive_Click);
+            // 
+            // btnSendStr
+            // 
+            this.btnSendStr.Location = new System.Drawing.Point(316, 126);
+            this.btnSendStr.Name = "btnSendStr";
+            this.btnSendStr.Size = new System.Drawing.Size(103, 23);
+            this.btnSendStr.TabIndex = 8;
+            this.btnSendStr.Text = "Send by string";
+            this.btnSendStr.UseVisualStyleBackColor = true;
+            this.btnSendStr.Click += new System.EventHandler(this.btnSendStr_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(342, 10);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 7;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lbReceived
+            // 
+            this.lbReceived.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbReceived.Location = new System.Drawing.Point(12, 152);
+            this.lbReceived.Name = "lbReceived";
+            this.lbReceived.Size = new System.Drawing.Size(407, 198);
+            this.lbReceived.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "CDM:";
+            // 
+            // tbCMD
+            // 
+            this.tbCMD.Location = new System.Drawing.Point(14, 99);
+            this.tbCMD.Name = "tbCMD";
+            this.tbCMD.Size = new System.Drawing.Size(405, 21);
+            this.tbCMD.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(143, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Port:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP:";
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(178, 12);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(100, 21);
+            this.tbPort.TabIndex = 1;
+            // 
+            // tbIP
+            // 
+            this.tbIP.Location = new System.Drawing.Point(37, 12);
+            this.tbIP.Name = "tbIP";
+            this.tbIP.Size = new System.Drawing.Size(100, 21);
+            this.tbIP.TabIndex = 0;
+            // 
+            // TCPDemoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(429, 388);
+            this.Controls.Add(this.panel1);
+            this.Name = "TCPDemoForm";
+            this.ShowIcon = false;
+            this.Text = "TCPDemoForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnReceive;
+        private System.Windows.Forms.Button btnSendStr;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label lbReceived;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbCMD;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.TextBox tbIP;
+        private System.Windows.Forms.Label lbStatus;
+        private System.Windows.Forms.Button btnDiscon;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lbTimer;
+        private System.Windows.Forms.Button btnSendBt;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbReadtimeout;
+        private System.Windows.Forms.TextBox tbInterval;
+        private System.Windows.Forms.RadioButton rbrn;
+        private System.Windows.Forms.RadioButton rbn;
+        private System.Windows.Forms.RadioButton rbr;
+    }
+}
---
+++ TCPClientDemo/TCPDemoForm.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCPClientDemo
+{
+    public partial class TCPDemoForm : Form
+    {
+        private TcpClient tcpClient;
+        private System.Timers.Timer timer;
+        private int sec;
+
+        public TCPDemoForm()
+        {
+            InitializeComponent();
+            initializeDemo();
+        }
+
+        private void initializeDemo()
+        {
+            tbIP.Text = Properties.Settings.Default.IP;
+            tbPort.Text = Properties.Settings.Default.Port.ToString();
+            tbInterval.Text = Properties.Settings.Default.SleepInterval.ToString();
+            tbReadtimeout.Text = Properties.Settings.Default.ReadTimeOut.ToString();
+            rbr.Checked = true;
+            timer = new System.Timers.Timer(1000);
+            timer.Elapsed += OnTimerElapsed;
+        }
+
+        private void OnTimerElapsed(object sender, EventArgs e)
+        {
+            if (!tcpClient.Connected)
+            {
+                lbStatus.Text = "Offline";
+                lbStatus.ForeColor = Color.Brown;
+                timer.Close();
+                lbTimer.Text = "Connecting: 0";
+            }
+            else
+            {
+                sec++;
+                lbTimer.Text = "Connecting: " + sec.ToString();
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tcpClient = new TcpClient();
+                tcpClient.Connect(tbIP.Text, Convert.ToInt32(tbPort.Text));
+                if (tcpClient.Connected)
+                {
+                    lbStatus.Text = "Online";
+                    lbStatus.ForeColor = Color.Green;
+                    lbReceived.Text = tbIP.Text + ": " + tbPort.Text + " connect successed.";
+                    sec = 0;
+                    timer.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                lbReceived.Text = ex.Message;
+            }
+            finally
+            {
+                UpdateSettings();
+            }
+        }
+
+        private void btnDiscon_Click(object sender, EventArgs e)
+        {
+            if (tcpClient == null)
+                return;
+
+            tcpClient.Close();
+
+            lbStatus.Text = "Offline";
+            lbStatus.ForeColor = Color.Brown;
+            lbReceived.Text = "Connection closed.";
+            timer.Close();
+            sec = 0;
+            lbTimer.Text = "Connecting: 0";
+        }
+
+        private void btnSendBt_Click(object sender, EventArgs e)
+        {
+            if (tcpClient == null)
+                return;
+            if (!tcpClient.Connected)
+            {
+                btnDiscon_Click(null, null);
+                return;
+            }
+
+            string newstr = string.Empty;
+            if(rbr.Checked)
+                newstr=tbCMD.Text + "\r";
+            else if(rbn.Checked)
+                newstr = tbCMD.Text + "\n";
+            else if(rbrn.Checked)
+                newstr = tbCMD.Text + "\r\n";
+
+            byte[] buffer = new byte[newstr.Length];
+            buffer = System.Text.Encoding.ASCII.GetBytes(newstr);
+
+            try
+            {
+                NetworkStream n = tcpClient.GetStream();
+                n.ReadTimeout = Properties.Settings.Default.ReadTimeOut;
+                n.Write(buffer, 0, buffer.Length);
+                Thread.Sleep(Properties.Settings.Default.SleepInterval);
+                byte[] buffer_read = new byte[1024];
+                n.Read(buffer_read, 0, 1024);
+                string str_read = System.Text.Encoding.ASCII.GetString(buffer_read);
+                lbReceived.Text = str_read;
+            }
+            catch (Exception ex)
+            {
+                lbReceived.Text = ex.Message;
+            }
+        }
+
+        private void btnSendStr_Click(object sender, EventArgs e)
+        {
+            if (tcpClient == null)
+                return;
+            if (!tcpClient.Connected)
+            {
+                btnDiscon_Click(null, null);
+                return;
+            }
+
+            string newstr = string.Empty;
+            if (rbr.Checked)
+                newstr = tbCMD.Text + "\r";
+            else if (rbn.Checked)
+                newstr = tbCMD.Text + "\n";
+            else if (rbrn.Checked)
+                newstr = tbCMD.Text + "\r\n";
+
+            try
+            {
+                NetworkStream n = tcpClient.GetStream();
+                n.ReadTimeout = Properties.Settings.Default.ReadTimeOut;
+                BinaryWriter bw = new BinaryWriter(n);
+                bw.Write(newstr);
+                bw.Flush();
+                Thread.Sleep(Properties.Settings.Default.SleepInterval);
+                lbReceived.Text = new BinaryReader(n).ReadString();
+            }
+            catch (Exception ex)
+            {
+                lbReceived.Text = ex.Message;
+            }
+        }
+
+        private void btnReceive_Click(object sender, EventArgs e)
+        {
+            if (tcpClient == null)
+                return;
+            if (!tcpClient.Connected)
+            {
+                btnDiscon_Click(null, null);
+                return;
+            }
+
+            string revicestr;
+            byte[] buffer = new byte[1024];
+
+            try
+            {
+                tcpClient.GetStream().ReadTimeout = Properties.Settings.Default.ReadTimeOut;
+                tcpClient.GetStream().Read(buffer, 0, buffer.Length);
+                revicestr = System.Text.Encoding.ASCII.GetString(buffer);
+
+                lbReceived.Text = revicestr;
+            }
+            catch (Exception ex)
+            {
+                lbReceived.Text = ex.Message;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lbReceived.Text = String.Empty;
+        }
+
+        private void OnRadiobuttonChecked(object sender,EventArgs e)
+        {
+            if (rbr.Checked)
+            {
+                rbn.Checked = false;
+                rbrn.Checked = false;
+            }
+            else if (rbn.Checked)
+            {
+                rbr.Checked = false;
+                rbrn.Checked = false;
+            }
+            else if (rbrn.Checked)
+            {
+                rbr.Checked = false;
+                rbn.Checked = false;
+            }
+        }
+
+        private void UpdateSettings()
+        {
+            Properties.Settings.Default.IP = tbIP.Text;
+            Properties.Settings.Default.Port = Convert.ToInt32(tbPort.Text);
+            Properties.Settings.Default.SleepInterval = Convert.ToInt32(tbInterval.Text);
+            Properties.Settings.Default.ReadTimeOut = Convert.ToInt32(tbReadtimeout.Text);
+        }
+    }
+}
